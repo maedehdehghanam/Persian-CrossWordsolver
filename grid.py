dc98600305622c48e38676ec203efd083f4ecf60
Maedeh <maedehdehghanam@gmail.com>
2023-01-07 16:05:34 +0330

    parse table's info

--- grid.py
+++ grid.py
@@ -11,7 +11,41 @@ n   = int(dim.split()[1])
 get the table's shape to make the grid
 '''
 shape = f.readline()
+#backwarding the string
+shape =''.join(reversed(shape))
 shape_list = list(shape)
-grid= np.array(shape_list[0:-1]).reshape((m,n)) 
+shape_list = shape_list[1:]
+grid= np.array(shape_list).reshape((m,n))
 
-print(grid)
+#backward every line of the table 
+for i in range(m):
+	grid[i] = np.flipud(grid[i])
+
+'''
+get the info
+'''
+info = f.readline()
+info_list = info.split('@')
+horizental_info = []
+vertical_info =[]
+for index,value in enumerate(info_list):
+	row = value.split('#')
+	row_list=[]
+	for i in range(len(row)):
+		key = row[i]
+		if key[0]=='&':
+			key = key[1:]
+		elif key[-1] == '&':
+			key = key[:-1]
+		data = {
+				"KEY" : key,
+				"VALUE": "",
+				}
+		row_list.append(data)
+	if index<m:
+		horizental_info.append(row_list)
+	else:
+		vertical_info.append(row_list)
+print(horizental_info)
+print("*******")
+print(vertical_info)
